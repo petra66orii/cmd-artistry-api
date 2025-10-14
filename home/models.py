25314be85e3b9f5b948345d5c08f793799518f23
Petra <petra66orii@gmail.com>
2025-10-14 18:56:28 +0100

    Add Testimonial model and migrate, #8

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+class Testimonial(models.Model):
+    """Represents a testimonial from a client."""
+    quote = models.TextField()
+    author_name = models.CharField(max_length=100)
+    company_or_title = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        help_text="e.g., 'Homeowner' or 'CEO of Cafe Name'"
+    )
+    is_approved = models.BooleanField(
+        default=False, 
+        help_text="Only approved testimonials will be shown on the site."
+    )
+    submitted_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    class Meta:
+        ordering = ['-submitted_at']
+
+    def __str__(self):
+        return f'"{self.quote[:30]}..." by {self.author_name}'
