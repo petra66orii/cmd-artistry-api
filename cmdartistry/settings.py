a6f10801eec11e69ec92c5d80c542436609dd385
Petra <petra66orii@gmail.com>
2025-10-14 18:32:22 +0100

    Create contact app, #1

--- cmdartistry/settings.py
+++ cmdartistry/settings.py
@@ -13,7 +13,10 @@ SECRET_KEY = 'django-insecure-ecix=7vui8w*1k1+qqq&6a(yfc==m$lh7-ksi&-$2(czd_*&cj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -28,11 +31,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'services',
+    'contact',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -111,3 +116,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
