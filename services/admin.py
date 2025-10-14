538a02f68f5fff072bfb3e67445e35e2f45d8c41
Petra <petra66orii@gmail.com>
2025-10-14 18:16:28 +0100

    Register Service model with admin, #6

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+# This customizes how the Service model appears in the admin
+class ServiceAdmin(admin.ModelAdmin):
+    # This shows the title and slug in the list view of services
+    list_display = ('title', 'slug')
+    
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Service, ServiceAdmin)
