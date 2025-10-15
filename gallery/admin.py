bfb15b90e46af4c5e74aacc82e5ae952690c56f9
Petra <petra66orii@gmail.com>
2025-10-15 18:42:33 +0100

    Update admin model to display image thumbnail, #5

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,8 +1,22 @@
+# gallery/admin.py
+
 from django.contrib import admin
+from django.utils.html import mark_safe
 from .models import GalleryItem
 
+@admin.register(GalleryItem)
 class GalleryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'created_at')
-    list_filter = ('category',) # This adds the filter sidebar
+    list_display = ('title', 'category', 'image_thumbnail', 'created_at')
+    list_filter = ('category',)
+    readonly_fields = ('image_thumbnail',)
 
-admin.site.register(GalleryItem, GalleryAdmin)
+    def image_thumbnail(self, obj):
+        """
+        Creates a small thumbnail of the image to display in the admin list.
+        """
+        if obj.image:
+            # Create an HTML img tag with a max width of 100px
+            return mark_safe(f'<img src="{obj.image.url}" width="100" />')
+        return "No Image" # Display this text if no image is uploaded
+    
+    image_thumbnail.short_description = 'Thumbnail' # Sets the column header name
