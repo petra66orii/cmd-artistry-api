7287e73ccb523d9293e6b72e351503edda16eb5d
Petra <petra66orii@gmail.com>
2025-10-15 16:07:20 +0100

    Register model with admin, #6

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import GalleryItem
 
-# Register your models here.
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at')
+    list_filter = ('category',) # This adds the filter sidebar
+
+admin.site.register(GalleryItem, GalleryAdmin)
