660af2a9710e78cf11c27bb31a1dca0db1884237
Petra <petra66orii@gmail.com>
2025-10-16 23:21:08 +0100

    Add 403 page view and URL

--- cmdartistry/urls.py
+++ cmdartistry/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import permission_denied_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +14,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler403 = permission_denied_view
